8568ce2660ae4ba15b0af888a553b6565a08441f
guzziThor <augustine.t@icloud.com>
2020-05-07 15:18:33 +0930

    task 004

---
+++ s290428-Thorbjornsen-Augustine/Sprint-2-Deliverables/Task 004/before.java
@@ -0,0 +1,31 @@
+class CheeseCakeFactory{
+     private string factory_Name;
+     private bool delivers;
+     private string cake_Size;
+     private string toppings;
+
+public string getFactory_Name() {
+        return this.factory_Name;
+    }
+    public bool delivers() {
+        return this.delivers;
+    }
+
+public string cake_Size() {
+        return this.cake_Size;
+    }
+
+public string toppings() {
+        return this.toppings;
+    }
+
+
+
+}
+
+
+
+
+#CREATE THE objects
+
+newrestaurant = pizzarestaurant3()
